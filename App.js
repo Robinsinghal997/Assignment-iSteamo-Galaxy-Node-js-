1aefaf5ed2a35d59f7657a1bb20d3584d266ff5d
Robin <85216957+Robinsinghal997@users.noreply.github.com>
2022-02-21 19:18:22 +0530

    SetUp the node Project

---
+++ App.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT 
+app.use(express.json())
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running On ${port}`);
+})
